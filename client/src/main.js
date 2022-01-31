7f967f190571cb73dee87149b9441e42835e331d
yulio64neos <julio64neos@gmail.com>
2022-01-31 17:52:14 -0600

    Agregue las dependencias del lado del cliente

--- client/src/main.js
+++ client/src/main.js
@@ -6,6 +6,13 @@ import vuetify from './plugins/vuetify'
 import '@babel/polyfill'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@fortawesome/fontawesome-free/css/all.css'
+import Vuelidate from 'vuelidate'
+Vue.use(Vuelidate);
+import VueSweetalert2 from 'vue-sweetalert2'
+Vue.use(VueSweetalert2);
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
 
--- server/src/config/database.js
+++ server/src/config/database.js
@@ -4,7 +4,7 @@ const mysqlConnection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
-  database: "SCC",
+  database: "kraid",
   multipleStatements: true,
 });
 
