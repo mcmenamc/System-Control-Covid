5930d42b9d8ededec46b5f11680d80aa4670ed95
yulio64neos <julio64neos@gmail.com>
2022-01-31 18:02:38 -0600

    Anexe los cambios de la nueva BD Kraid

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,21 +1,12 @@
 const mysql = require('mysql')
 
 const mysqlConnection = mysql.createConnection({
-<<<<<<< HEAD
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'SCCovid',
-  multipleStatements: true
-})
-=======
   host: "localhost",
   user: "root",
   password: "",
   database: "kraid",
   multipleStatements: true,
 });
->>>>>>> 7f967f190571cb73dee87149b9441e42835e331d
 
 mysqlConnection.connect(function (err) {
   if (err) {
