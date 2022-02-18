178e6885f150a570f17f00bd90fec753d9c76eb8
mcmenamc <mcmena636@gmail.com>
2022-02-17 20:44:15 -0600

    Pagina de inicio Agregada

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,55 +1,192 @@
 <template>
   <v-app>
     <v-app-bar
+      v-if="$vuetify.breakpoint.mdAndUp"
+      elevation="0"
       app
-      color="primary"
-      dark
+      :color="bgcolor"
+      class="d-flex justify-center"
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
+      <!-- <v-toolbar-side-icon class="mr-5"> -->
+      <v-img width="180" class="mr-5" src="@/assets/images/logo.png"></v-img>
+      <!-- </v-toolbar-side-icon>   -->
       <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
+        class="mr-2 custom-transform-class text-none text-md-h6"
+        :text="link.btn"
+        v-for="link of links"
+        :key="link.id"
+        :to="link.url"
+        :color="link.color"
+        :plain="link.btn"
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>fas fa-external-link-alt</v-icon>
+        {{ link.name }}
       </v-btn>
     </v-app-bar>
+    <v-app-bar
+      elevation="0"
+      v-if="$vuetify.breakpoint.smAndDown"
+      app
+      :color="bgcolor"
+      class=""
+    >
+      <v-row class="flex justify-center align-center">
+        <v-col cols="5" sm="4" class="d-flex justify-start">
+          <v-img width="150" src="@/assets/images/logo.png"></v-img>
+        </v-col>
+        <v-col cols="5" sm="4" class="d-flex justify-end">
+          <v-btn @click.stop="drawer = !drawer" icon>
+            <v-icon>{{ `fas ${drawer ? "fa-times" : "fa-bars"}` }}</v-icon>
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-app-bar>
+    <v-navigation-drawer v-model="drawer" app temporary>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6"
+            >SISTEMA-COVID-19</v-list-item-title
+          >
+        </v-list-item-content>
+      </v-list-item>
+      <v-divider></v-divider>
+      <v-list nav dense>
+        <v-list-item-group active-class="primary--text text--accent-4">
+          <v-list-item
+            v-for="link in links"
+            :key="link.id"
+            :to="link.url"
+            :class="`${link.btn ? '' : 'd-none'}`"
+          >
+            <v-list-item-icon>
+              <v-icon>{{ link.icon }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>{{ link.name }}</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+      <template v-slot:append>
+        <div class="pa-2">
+          <v-btn :to="links[4].url" :color="links[4].color" block>
+            {{ links[4].name }}
+          </v-btn>
+        </div>
+        <div class="pa-2">
+          <v-btn :to="links[5].url" :color="links[5].color" block>
+            {{ links[5].name }}
+          </v-btn>
+        </div>
+      </template>
+    </v-navigation-drawer>
 
     <v-main>
-      <router-view/>
+      <v-container fluid>
+        <router-view />
+      </v-container>
     </v-main>
+
+    <v-footer>
+      <span>footer</span>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
+  name: "App",
 
   data: () => ({
-    //
+    links: [
+      {
+        name: "Home",
+        url: "/",
+        btn: true,
+        color: "primary",
+        icon: "fas fa-home",
+      },
+      {
+        name: "Acerca de",
+        url: "/about",
+        btn: true,
+        color: "primary",
+        icon: "fas fa-question-circle",
+      },
+      {
+        name: "Contacto",
+        url: "/contact",
+        btn: true,
+        color: "primary",
+        icon: "fas fa-info-circle",
+      },
+      {
+        name: "Sintomas",
+        url: "/sintomas",
+        btn: true,
+        color: "primary",
+        icon: "fas fa-ambulance",
+      },
+      {
+        name: "Entrar",
+        url: "/login",
+        btn: false,
+        color: "primary",
+      },
+      {
+        name: "Registrar",
+        url: "/register",
+        btn: false,
+        color: "success",
+      },
+    ],
+    bgcolor: "transparent",
+    drawer: false,
   }),
+  mounted() {
+    window.onscroll = () => {
+      this.changeColor();
+    };
+  },
+  methods: {
+    changeColor() {
+      if (
+        document.body.scrollTop > 75 ||
+        document.documentElement.scrollTop > 75
+      ) {
+        this.bgcolor = "white";
+      } else {
+        this.bgcolor = "transparent";
+      }
+    },
+  },
 };
 </script>
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Oswald:wght@700&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Oswald:wght@700&family=Roboto:wght@900&display=swap");
+@import url('https://fonts.googleapis.com/css2?family=Mukta:wght@500&display=swap');
+.text-one {
+  font-family: "Oswald", sans-serif;
+}
+.text-sub {
+  font-family: "Roboto", sans-serif;
+}
+.text-normal{
+  font-family: 'Mukta', sans-serif;
+}
+.rojo-text {
+  color: #ff8086;
+}
+main {
+  background-color: #f6f8fe;
+}
+html::-webkit-scrollbar {
+  width: 8px;
+  background: #f0f0f0;
+}
+html::-webkit-scrollbar-thumb {
+  background-color: rgb(206, 206, 206);
+  border-radius: 10px;
+}
+html::-webkit-scrollbar-thumb:hover {
+  background-color: rgba(105, 105, 105, 0.7);
+}
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,15 +1,130 @@
-<template>
-  <hello-world />
+<template >
+  <div>
+    <v-img
+      :height="`${$vuetify.breakpoint.mobile ? 'auto' : '92vh'}`"
+      src="@/assets/images/fondo.jpg"
+    >
+      <v-row
+        :class="`img-fondo d-flex flex-wrap justify-center ${
+          $vuetify.breakpoint.mobile ? 'mt-4' : 'mt-16'
+        }`"
+      >
+        <v-col cols="12" md="11" lg="9" xl="8">
+          <v-row class="align-center">
+            <v-col md="6" class="">
+              <v-row align="center" justify="center">
+                <v-col cols="10" class="">
+                  <v-img src="@/assets/images/coronovirus.jpg"></v-img>
+                </v-col>
+                <v-row class="justify-center">
+                  <v-col cols="10" class="my-3">
+                    <p class="text-justify text-normal">
+                      El coronavirus (COVID-19) se informó por primera vez en
+                      Wuhan, Hubei, China, en diciembre de 2019; el brote fue
+                      reconocido posteriormente como una pandemia por la
+                      Organización Mundial de la Salud (OMS) el 11 de marzo de
+                      2020.
+                    </p>
+                  </v-col>
+                </v-row>
+                <v-row class="d-flex justify-center">
+                  <v-col cols="auto" sm="4" md="5"  lg="4">
+                    <v-btn rounded color="primary" class="text-none"
+                      >Como Protegerse
+                      <v-icon right>fas fa-shield-virus</v-icon>
+                    </v-btn>
+                  </v-col>
+                  <v-col cols="auto" sm="4" md="5" lg="4">
+                    <v-btn
+                      rounded
+                      href="https://previenecovid19.puebla.gob.mx/"
+                      target="_blank"
+                      text
+                      color="primary"
+                      >Acerca de Covid-19 🡢
+                    </v-btn>
+                  </v-col>
+                </v-row>
+              </v-row>
+              <v-row class="d-flex justify-center my-5">
+                <v-col align="center" cols="12" class="text-one txt-size"
+                  >Casos Covid-19</v-col
+                >
+                <v-col
+                  align="center"
+                  cols="auto"
+                  sm="4"
+                  class="text-one rojo-text txt-letters"
+                  >MUNDIAL
+                  <p class="grey--text">{{ covid.cases.toLocaleString() }}</p>
+                </v-col>
+                <v-col
+                  cols="auto"
+                  sm="4"
+                  class="text-one rojo-text txt-letters"
+                >
+                  FALLECIDOS
+                  <p class="grey--text">
+                    {{ covid.deaths.toLocaleString() }}
+                  </p></v-col
+                >
+                <v-col cols="auto" sm="4" class="text-one rojo-text txt-letters"
+                  >RECOVERED
+                  <p class="grey--text">
+                    {{ covid.recovered.toLocaleString() }}
+                  </p></v-col
+                >
+              </v-row>
+            </v-col>
+            <v-col md="6" class="">
+              <v-img contain src="@/assets/images/imageb5.png"></v-img>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-img>
+    <v-row class="my-10 d-flex justify-center white">
+      <v-col cols="12" md="11" lg="9" xl="7">
+        <v-row class="my-16">
+          <v-col cols="7" class="error"> </v-col>
+          <v-col cols="5" class="info"> </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
-
-  export default {
-    name: 'Home',
-
-    components: {
-      HelloWorld,
+export default {
+  name: "Home",
+  data: () => ({
+    covid: {
+      cases: 0,
+      deaths: 0,
+      recovered: 0,
     },
-  }
+    dialog: false,
+  }),
+  created() {
+    this.getCovid();
+  },
+  methods: {
+    getCovid() {
+      this.axios
+        .get("https://coronavirus-19-api.herokuapp.com/all")
+        .then((response) => {
+          this.covid = response.data;
+        });
+    },
+  },
+};
 </script>
+<style scoped>
+.txt-size {
+  font-size: 38px;
+  color: rgb(114, 114, 114);
+}
+.txt-letters {
+  font-size: 27px;
+}
+</style>
