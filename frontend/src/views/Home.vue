97c0fc329e46bc3334ca16890e740fe39152e292
mcmenamc <mcmena636@gmail.com>
2022-03-01 00:51:58 -0600

    add  skeleton loader

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -7,6 +7,7 @@
       class="whatsapp success"
       ><v-icon color="white" size="45">fab fa-whatsapp</v-icon>
     </v-btn>
+
     <v-img
       :height="`${$vuetify.breakpoint.mobile ? 'auto' : '92vh'}`"
       src="@/assets/images/fondo.jpg"
@@ -17,14 +18,27 @@
         }`"
       >
         <v-col cols="12" md="11" lg="9" xl="8">
-          <v-row class="align-center">
-            <v-col md="6" class="">
+          <v-row class="d-flex justify-center">
+            <v-col cols="12" md="6">
               <v-row align="center" justify="center">
                 <v-col cols="10" class="">
-                  <v-img src="@/assets/images/coronovirus.jpg"></v-img>
+                  <v-skeleton-loader
+                    width="100%"
+                    :height="`${$vuetify.breakpoint.mobile ? '75' : '130'}`"
+                    type="image"
+                    :loading="loading"
+                    :class="`${$vuetify.breakpoint.mobile ? '' : 'my-8'}`"
+                  >
+                    <v-img src="@/assets/images/coronovirus.jpg"></v-img>
+                  </v-skeleton-loader>
                 </v-col>
-                <v-row class="justify-center">
-                  <v-col cols="10" class="my-3">
+                <v-col cols="10" class="my-3">
+                  <v-skeleton-loader
+                    type="text@5"
+                    :loading="loading"
+                    width="100%"
+                    :class="`${loading ? 'mt-2': ''}`"
+                  >
                     <p class="text-justify text-normal">
                       El coronavirus (COVID-19) se informó por primera vez en
                       Wuhan, Hubei, China, en diciembre de 2019; el brote fue
@@ -32,39 +46,63 @@
                       Organización Mundial de la Salud (OMS) el 11 de marzo de
                       2020.
                     </p>
-                  </v-col>
-                </v-row>
+                  </v-skeleton-loader>
+                </v-col>
                 <v-row class="d-flex justify-center">
-                  <v-col cols="auto" sm="4" md="5" lg="4" class="">
-                    <v-btn rounded color="primary" class="text-none"
-                      >Como Protegerse
-                      <v-icon right>fas fa-shield-virus</v-icon>
-                    </v-btn>
+                  <v-col
+                    cols="auto"
+                    sm="4"
+                    md="5"
+                    lg="4"
+                    class="d-flex justify-center"
+                  >
+                    <v-skeleton-loader
+                      type="button"
+                      :loading="loading"
+                      :class="`${loading ? 'mt-3': ''}`"
+                    >
+                      <v-btn rounded color="primary" class="text-none"
+                        >Como Protegerse
+                        <v-icon right>fas fa-shield-virus</v-icon>
+                      </v-btn>
+                    </v-skeleton-loader>
                   </v-col>
-                  <v-col cols="auto" sm="4" md="5" lg="4" class="ml-2">
-                    <v-btn
-                      rounded
-                      href="https://previenecovid19.puebla.gob.mx/"
-                      target="_blank"
-                      text
-                      color="primary"
-                      >Acerca de Covid-19
-                      <v-icon small right>fas fa-arrow-right</v-icon>
-                    </v-btn>
+                  <v-col
+                    cols="auto"
+                    sm="4"
+                    md="5"
+                    lg="4"
+                    class="d-flex justify-center"
+                  >
+                    <v-skeleton-loader
+                      type="button"
+                      :loading="loading"
+                      :class="`${loading ? 'mt-3': ''}`"
+                    >
+                      <v-btn
+                        rounded
+                        href="https://previenecovid19.puebla.gob.mx/"
+                        target="_blank"
+                        text
+                        color="primary"
+                      >
+                        Acerca de Covid-19
+                        <v-icon small right>fas fa-arrow-right</v-icon>
+                      </v-btn>
+                    </v-skeleton-loader>
                   </v-col>
                 </v-row>
               </v-row>
               <v-row class="d-flex justify-center my-5">
-                <v-col align="center" cols="12" class="text-one txt-size"
-                  >Casos Covid-19</v-col
-                >
-                <v-col
-                  align="center"
-                  cols="auto"
-                  sm="4"
-                  class="text-one rojo-text txt-letters"
-                  >MUNDIAL
-                  <p class="grey--text">{{ covid.cases.toLocaleString() }}</p>
+                <v-col align="center" cols="12" class="text-one txt-size">
+                  <v-skeleton-loader
+                    width="60%"
+                    type="text"
+                    :loading="loading"
+                    :class="`${loading ? 'mt-3': ''}`"
+                  >
+                    Casos Covid-19
+                  </v-skeleton-loader>
                 </v-col>
                 <v-col
                   align="center"
@@ -72,25 +110,83 @@
                   sm="4"
                   class="text-one rojo-text txt-letters"
                 >
-                  FALLECIDOS
-                  <p class="grey--text">
-                    {{ covid.deaths.toLocaleString() }}
-                  </p></v-col
-                >
+                  <v-skeleton-loader
+                    type="text"
+                    :loading="loading"
+                    width="100"
+                    :class="`${loading ? 'mt-3': ''}`"
+                  >
+                    MUNDIAL
+                  </v-skeleton-loader>
+                  <v-skeleton-loader
+                    type="text"
+                    :loading="loading"
+                    width="150"
+                    :class="`${loading ? 'mt-6': ''}`"
+                  >
+                    <p class="grey--text">{{ covid.cases.toLocaleString() }}</p>
+                  </v-skeleton-loader>
+                </v-col>
+                <v-col
+                  align="center"
+                  cols="auto"
+                  sm="4"
+                  class="text-one rojo-text txt-letters"
+                  ><v-skeleton-loader
+                    type="text"
+                    :loading="loading"
+                    width="100"
+                    :class="`${loading ? 'mt-3': ''}`"
+                    >FALLECIDOS
+                  </v-skeleton-loader>
+                  <v-skeleton-loader
+                    type="text"
+                    :loading="loading"
+                    width="150"
+                    :class="`${loading ? 'mt-6': ''}`"
+                  >
+                    <p class="grey--text">
+                      {{ covid.deaths.toLocaleString() }}
+                    </p>
+                  </v-skeleton-loader>
+                </v-col>
                 <v-col
                   cols="auto"
                   align="center"
                   sm="4"
                   class="text-one rojo-text txt-letters"
-                  >RECUPERADOS
-                  <p class="grey--text">
-                    {{ covid.recovered.toLocaleString() }}
-                  </p></v-col
                 >
+                  <v-skeleton-loader
+                    type="text"
+                    :loading="loading"
+                    width="100"
+                    :class="`${loading ? 'mt-3': ''}`"
+                    >RECUPERADOS
+                  </v-skeleton-loader>
+                  <v-skeleton-loader
+                    type="text"
+                    :loading="loading"
+                    width="150"
+                    :class="`${loading ? 'mt-6': ''}`"
+                    ><p class="grey--text">
+                      {{ covid.recovered.toLocaleString() }}
+                    </p>
+                  </v-skeleton-loader>
+                </v-col>
               </v-row>
             </v-col>
-            <v-col md="6" class="">
-              <v-img contain src="@/assets/images/imageb5.png"></v-img>
+            <v-col cols="10" sm="8" md="6" class="pa-0">
+              <v-skeleton-loader
+                type="image, image, image"
+                :loading="loading"
+                :class="`${loading ? 'mt-2': ''}`"
+              >
+                <v-img
+                  contain
+                  height="100%"
+                  src="@/assets/images/imageb5.png"
+                ></v-img>
+              </v-skeleton-loader>
             </v-col>
           </v-row>
         </v-col>
@@ -118,6 +214,7 @@ export default {
       recovered: 0,
     },
     dialog: false,
+    loading: true,
   }),
   async created() {
     this.getCovid();
@@ -128,7 +225,11 @@ export default {
       this.axios
         .get("https://coronavirus-19-api.herokuapp.com/all")
         .then((response) => {
+          this.loading = false;
           this.covid = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
         });
     },
   },
